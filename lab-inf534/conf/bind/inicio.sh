8f1629cff7a1e9e3c9c290dfbb8b9d74e9ccf8ef
Sidney Pio de Campos <sidneypio@gmail.com>
2022-07-11 21:23:18 -0300

    docker com servidor bind

---
+++ lab-inf534/conf/bind/inicio.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# para Alpine Linux
+# https://wiki.alpinelinux.org/wiki/Setting_up_a_ssh-server
+# Rodar da primeira vez:
+#ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+#ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+# criando usuario inf500 com senha inf500
+#adduser -D --home /home/inf500 --shell /bin/ash -g "usuario para teste inf500" inf500
+#echo "inf500:inf500" |chpasswd
+# iniciando ssh server
+#/usr/sbin/sshd 
+
+
+# Para Debian
+# criando usuario inf500 com senha inf500
+#adduser --home /home/inf500 --shell /bin/bash --disabled-password --gecos "usuario para teste inf500" inf500
+
+# mudando a senha do usuario inf500 para inf500
+#echo "inf500:inf500" |chpasswd
+
+# iniciando ssh server
+#/usr/bin/ssh-keygen -A
+#/etc/init.d/ssh  start
+
+# Removendo rota default 
+#ip route del default
+#ip -6 route del default
+
+# ajustando rota
+ip route add 10.10.0.0/16 via 10.10.100.100 
+ip route add 2001:db8:2021:10::/64 via 2001:db8:2021:100::100 
+ip route add 2001:db8:2021:50::/64 via 2001:db8:2021:100::100 
+
+# inicializando o bind daemon
+/usr/sbin/rndc-confgen -a
+/usr/sbin/named -c /etc/bind/named.conf
+
+while true; do sleep 10; done
--- lab-inf534/home/ssh/inf500/.bash_history
+++ lab-inf534/home/ssh/inf500/.bash_history
@@ -2,3 +2,8 @@ w
 exit
 w
 exit
+exit
+w
+ps -ef
+ip a s
+exit
