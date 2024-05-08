e8535623574d14577f207c8afcc7e9f7b3517ab5
Sidney Pio de Campos <sidneypio@gmail.com>
2024-05-08 20:15:53 -0300

    ajuste no Makefile das imagens e ajustando inicializacao do sensor

--- lab-inf534/conf/sensor/inicio.sh
+++ lab-inf534/conf/sensor/inicio.sh
@@ -9,5 +9,7 @@ ip -6 route del default
 ip route add 10.10.0.0/16 via 10.10.10.100 
 ip route add 2001:db8:2021:100::/64 via 2001:db8:2021:10::100
 
+# Esperando 2 minutos para tudo inicializar corretamente
+sleep 120
 #while true; do sleep 10; done
 /app/sensor.py
